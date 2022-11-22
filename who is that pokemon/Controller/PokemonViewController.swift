3983c81c05fa7138cc2cbaf0953fa392cdf31cb2
Lucas Suarez <lucas.suarez.dev@gmail.com>
2022-11-22 10:14:23 -0400

    added manager for imageData

--- who is that pokemon/Controller/PokemonViewController.swift
+++ who is that pokemon/Controller/PokemonViewController.swift
@@ -23,9 +23,17 @@ class PokemonViewController: UIViewController {
     
     // agregamos el manager
     lazy var pokemonManager = PokemonManager()
+    lazy var imageManager = ImageManager()
+    
+    var random4Pokemon : [PokemonModel] = []
+    var correctAnswer : String = ""
+    var correctAnswerImage : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonManager.delegate = self
+        imageManager.delegate = self
+        
         decorationButton()
         pokemonManager.fetchPokemon()
         
@@ -53,10 +61,27 @@ class PokemonViewController: UIViewController {
     
 }
 
+extension PokemonViewController : ImageManagerDelegate {
+    func didFailWithErrorImage(error: Error) {
+        print(error)
+    }
+    
+    func didUpdateImage(image: ImageModel) {
+        print("IMAGEN: \(image.imageUrl)")
+    }
+    
+    
+}
+
 extension PokemonViewController : PokemonManagerDelegate {
     
     func didUpdatePokemon(pokemons: [PokemonModel]) {
-        print(pokemons)
+        random4Pokemon = pokemons.choose(4)
+        let index = Int.random(in: 0...3)
+        let imageData = random4Pokemon[index].imageUrl
+        imageManager.fetchImage(url: imageData)
+        
+        print(pokemons.choose(4))
     }
     
     func didFailWithError(error: Error) {
@@ -64,4 +89,16 @@ extension PokemonViewController : PokemonManagerDelegate {
     }
     
     
+}
+
+extension Collection where Indices.Iterator.Element == Index {
+    public subscript(safe index: Index) -> Iterator.Element? {
+        return ( startIndex <= index && index < endIndex ) ? self[index] : nil
+    }
+}
+
+extension Collection {
+    func choose(_ n: Int) -> Array<Element> {
+        Array(shuffled().prefix(n))
+    }
 }
--- who is that pokemon/Model/ImageManager.swift
+++ who is that pokemon/Model/ImageManager.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+protocol ImageManagerDelegate {
+    func didUpdateImage(image: ImageModel)
+    func didFailWithErrorImage(error: Error)
+}
+
+struct ImageManager {
+    
+    var delegate : ImageManagerDelegate?
+    
+    func fetchImage(url : String) {
+        performRequest(with: url)
+    }
+    
+    private func performRequest(with urlString: String) {
+        // 1. Create/get URL
+        if let url = URL(string: urlString) {
+            // 2. Create the URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    self.delegate?.didFailWithErrorImage(error: error!)
+                }
+                if let safeData = data {
+                    if let imagen = self.parseJSON(imageData: safeData) {
+                        self.delegate?.didUpdateImage(image: imagen)
+                    }
+                }
+            }
+            
+            // 4. Start the task
+            task.resume()
+            
+        }
+    }
+    
+    private func parseJSON(imageData: Data) -> ImageModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(ImageData.self, from: imageData)
+            let image = decodedData.sprites?.other?.officialArtwork?.frontDefault ?? ""
+            
+            return ImageModel(imageUrl: image)
+        } catch {
+            return nil
+        }
+    }
+    
+}
